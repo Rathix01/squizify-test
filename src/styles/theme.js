6b3b1944cb701e9d6f19ae6ac77494c6a3102d00
Tim <timothy.coenradi@rocos.io>
2020-08-07 09:06:33 +1200

    removed unused values

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,20 +3,6 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 // A custom theme for this app
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
   brand: {
     main: "rgb(9, 211, 172)",
     radius: "8px",
